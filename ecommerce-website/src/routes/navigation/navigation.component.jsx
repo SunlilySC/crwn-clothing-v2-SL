dd58b3f5b800d5c7aaba181c82326a1ef51de50d
SunlilySC <166149844+SunlilySC@users.noreply.github.com>
2024-07-25 14:44:58 -0600

    added in our addToCart feature

--- ecommerce-website/src/routes/navigation/navigation.component.jsx
+++ ecommerce-website/src/routes/navigation/navigation.component.jsx
@@ -2,16 +2,14 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
-import { signOutUser } from '../../utils/firebase/firebase.utils';
-
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.components';
-
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.components';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
@@ -30,6 +28,7 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
+          
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>
               SIGN OUT
